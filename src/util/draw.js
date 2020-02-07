f5b20ca081cd33a2fd362a4b7955f61fa79f93aa
서준배[FE플랫폼] <s.junbae@navercorp.com>
2020-02-07 18:34:32 +0900

    [Add] blazeface를 이용해 얼굴인식, 특정지점 그리기
    blazeface API를 이용해 눈,코,입, 얼굴영역 위치를 얻는다
    이 위치를 canvas를 이용해 그리기를 수행
    - 정확한 위치를 알기 어렵다.

---
+++ src/util/draw.js
@@ -0,0 +1,47 @@
+/**
+ * 이미지를 canvas에 옮겨 칠하기
+ * 
+ * @param {*} canvas 
+ * @param {*} image 
+ */
+function drawImg2Canvas(canvas,image){
+    const {width, height} = image;
+    canvas.width = width;
+    canvas.height = height;
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(image, 0, 0, width, height);
+}
+/**
+ * 얼굴 영역 칠하기
+ * 
+ * @param {} canvas 
+ * @param {*} image 
+ * @param  {...any} area 
+ */
+export function drawFacearea(canvas,image,...area){
+    drawImg2Canvas(canvas,image);
+    const ctx = canvas.getContext('2d');
+    ctx.globalAlpha = 0.2;
+    ctx.fillRect(...area);
+}
+function drawEye(canvas,...position){
+    const ctx = canvas.getContext('2d');
+    ctx.globalAlpha = 0.2;
+    ctx.fillStyle="red"
+    let x=position[0]-10;
+    let y=position[1]-10;
+    let width=20
+    let height=20
+    ctx.fillRect(x,y,width,height);
+}
+/**
+ * 양쪽 눈 그리기
+ * 
+ * @param {*} canvas 
+ * @param  {...any} area 
+ */
+export function drawEyes(canvas,...positions){
+    // drawImg2Canvas(canvas,image);
+    drawEye(canvas,positions[0],positions[1])
+    drawEye(canvas,positions[2],positions[3])
+}
