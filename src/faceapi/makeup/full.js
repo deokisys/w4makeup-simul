287d02c58584a330d7243d2f7df14335f4640b9e
서준배[FE플랫폼] <s.junbae@navercorp.com>
2020-02-12 15:51:59 +0900

    [Add] 각 부위별 밝기 조절 버튼추가
    - 각 부위마다 투명도를 조절하여 좀더 자연스러운 화장을 보여줍니다.
    - 투명도는 0에서 1사이로 버튼을 눌러서 선택하도록 하였습니다.

--- src/faceapi/makeup/full.js
+++ src/faceapi/makeup/full.js
@@ -2,6 +2,6 @@ import { drawImg2Canvas, drawLip,drawBlusher } from '../../util/draw.js';
 
 export default function fullmakeup(input,output,landmarks,...colors){
     drawImg2Canvas(output, input);
-    drawLip(output, colors[0], landmarks.slice(48, 68))
-    drawBlusher(output, colors[1], landmarks)
+    drawLip(output, colors[0],colors[1], landmarks.slice(48, 68))
+    drawBlusher(output, colors[2],colors[3], landmarks)
 }
